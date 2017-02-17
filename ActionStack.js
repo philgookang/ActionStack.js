7c55fbeb4142300696dabbe3e840e6b9b91a9e84
Phil Goo Kang <philgookang@gmail.com>
2017-02-17 16:26:26 +0900

    added action stack code

---
+++ ActionStack.js
@@ -0,0 +1,43 @@
+/**
+ * Action Stack.js
+ *
+ * Copyright 2017, Philgookang
+ * GNU General Public License v3.0
+ *
+ * If you found bug, please contact me via email <philgookang@gmail.com>
+ *
+ * @author Phil Goo Kang aka piggybank
+ * @version 0.0.1
+ * @url https://github.com/gromo/jquery.scrollbar/
+ *
+ */
+var ActionStack = (function() {
+
+	// create object, with a default empty queue
+	var that = {
+		"_queue" : []
+	};
+
+	/**
+	 * Add function to waiting stack line
+	 * @param {Function} callback_function this object(function) is called back at the end of the page
+	 */
+	that.add = function( callback_function ) {
+		// add cabllback function to the stack
+		that._queue.push(callback_function);
+	};
+
+	/**
+	 * Adds two numbers
+	 * @param {Function} callback_function this object(function) is called back at the end of the page
+	 */
+	that.process = function() {
+
+		// loop through the queue
+		for( var  i = 0; i < that._queue.length; i++ ) {
+
+			// run search object(function) in the queue
+			(that._queue[i])();
+		}
+	};
+})();
